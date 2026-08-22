351b0b408cbd5782535b68364470409e88baf1c6
pyckapatch-beep <pyckapatch@gmail.com>
2026-08-22 16:09:54 +0200

    Add files via upload

--- MineItServer/server.js
+++ MineItServer/server.js
@@ -5,6 +5,11 @@ const path = require("path");
 
 const app = express();
 
+app.use((req,res,next) => {
+    console.log(req.method,req.url);
+    next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("../"));
